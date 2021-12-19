82d88c5e50d82e65d49ddfb28b49f8c44fc0f07b
Lukáš <48251906+Gennario@users.noreply.github.com>
2021-12-19 13:06:58 +0100

    1.1 update
    Completely rewritten code.
    Migration from mojang web api to minetools.eu api. (Does not limit)
    Add labels to methods.
    Class converted to Util class

--- HeadSystem.java
+++ HeadSystem.java
@@ -1,6 +1,7 @@
+package cz.gennario.library.other;
+
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
-import org.bukkit.Bukkit;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.json.simple.JSONArray;
@@ -8,78 +9,81 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.lang.reflect.Method;
+import java.lang.reflect.Field;
 import java.net.URL;
-import java.util.Scanner;
 import java.util.UUID;
 
-@Getter
-public class HeadManager {
+/**
+ * HeadSystem [1.1]
+ *
+ * Util class that solves a bug with gaining a player's head on versions 1.16 and higher.
+ * Created by Gennario with <3
+ */
+public final class HeadManager {
 
+    /**
+     * Generation head type enum
+     */
     public enum HeadType {
         PLAYER_HEAD,
         BASE64;
     }
 
-    private HeadType type;
-    private String value;
-
-    public HeadManager(HeadType type, String value) {
-        this.type = type;
-        this.value = value;
-    }
-
-    public ItemStack convert() {
+    /**
+     * With this method you can get a player's head by nickname or a base64 head by base64 code
+     * @param type Determines whether you want to get the head by name or by base64
+     * @param value If you want a player's head, then the player's name. If you want base64, then base64 code.
+     * @return Head itemStack
+     */
+    public static ItemStack convert(HeadType type, String value) {
+        String texture = getPlayerHeadTexture(value);
         if (type.equals(HeadType.PLAYER_HEAD)) {
-            return getSkullByTexture(getPlayerHeadTexture(value));
+            return getSkullByTexture(texture);
         } else {
             return getSkullByTexture(value);
         }
     }
 
     private static ItemStack getSkullByTexture(String url) {
-        ItemStack head = getHead();
+        ItemStack head = MaterialUtils.getAllVersionStack("SKULL_ITEM", "PLAYER_HEAD", 3);
         if (url.isEmpty() || url.equals("none")) return head;
 
-        SkullMeta skullMeta = (SkullMeta) head.getItemMeta();
-        GameProfile profile = new GameProfile(UUID.randomUUID(), null);
-
+        SkullMeta meta = (SkullMeta) head.getItemMeta();
+        GameProfile profile = new GameProfile(UUID.randomUUID(), "");
         profile.getProperties().put("textures", new Property("textures", url));
-
+        Field profileField = null;
         try {
-            Method mtd = skullMeta.getClass().getDeclaredMethod("setProfile", GameProfile.class);
-            mtd.setAccessible(true);
-            mtd.invoke(skullMeta, profile);
-        } catch (Exception ex) {
-            ex.printStackTrace();
+            profileField = meta.getClass().getDeclaredField("profile");
+            profileField.setAccessible(true);
+            profileField.set(meta, profile);
+        } catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
+            e.printStackTrace();
         }
-
-        head.setItemMeta(skullMeta);
+        head.setItemMeta(meta);
         return head;
     }
 
-    public String getPlayerHeadTexture(String username) {
+    private static String getPlayerHeadTexture(String username) {
         if (getPlayerId(username).equals("none")) return "none";
-        String url = "https://sessionserver.mojang.com/session/minecraft/profile/" + getPlayerId(username);
+        String url = "https://api.minetools.eu/profile/" + getPlayerId(username);
         try {
             JSONParser jsonParser = new JSONParser();
             String userData = readUrl(url);
             Object parsedData = jsonParser.parse(userData);
 
             JSONObject jsonData = (JSONObject) parsedData;
-            JSONArray streamArray = (JSONArray) jsonData.get("properties");
-            JSONObject jsonMap = (JSONObject) streamArray.get(0);
+            JSONObject decoded = (JSONObject) jsonData.get("raw");
+            JSONArray textures = (JSONArray) decoded.get("properties");
+            JSONObject data = (JSONObject) textures.get(0);
 
-            return jsonMap.get("value").toString();
+            return data.get("value").toString();
         } catch (Exception ex) {
-            ex.printStackTrace();
+            return "none";
         }
-        return "none";
     }
 
-    private String readUrl(String urlString) throws Exception {
+    private static String readUrl(String urlString) throws Exception {
         BufferedReader reader = null;
         try {
             URL url = new URL(urlString);
@@ -95,34 +99,19 @@ public class HeadManager {
     }
 
 
-    private String getPlayerId(String playerName) {
+    private static String getPlayerId(String playerName) {
         try {
-            String url = "https://api.mojang.com/users/profiles/minecraft/" + playerName;
-            InputStream inputStream = new URL(url).openStream();
-            Scanner scanner = new Scanner(inputStream);
-            String data = scanner.nextLine();
-            JSONObject object = (JSONObject) new JSONParser().parse(data);
-            if (object.containsKey("id")) {
-                return object.get("id").toString();
-            }
-        } catch (Exception ignored) {
+            String url = "https://api.minetools.eu/uuid/" + playerName;
+            JSONParser jsonParser = new JSONParser();
+            String userData = readUrl(url);
+            Object parsedData = jsonParser.parse(userData);
+
+            JSONObject jsonData = (JSONObject) parsedData;
+
+            if(jsonData.get("id") != null) return jsonData.get("id").toString();
+            return "";
+        } catch (Exception ex) {
             return "none";
         }
-        return "none";
     }
-
-    public ItemStack getHead() {
-        ItemStack headStack = null;
-        Material material = null;
-        int data = 0;
-        try {
-            material = Material.valueOf("PLAYER_HEAD");
-        } catch (Exception e) {
-            material = Material.valueOf("SKULL_ITEM");
-            data = 3;
-        }
-        headStack = new ItemStack(material, 1, (byte)data);
-        return headStack;
-    }
-
 }
